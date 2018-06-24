ce040b62360f27b85ca0eb89e972f46c361a7a17
kalpesh05 <kalpeswhbhad57@gail.com>
2018-06-24 07:40:57 -0700

    todo list with redux

---
+++ src/components/TodoList.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Todo from './Todo';
+
+const TodoList = ({ todos, onTodoClick }) => (
+  <div>
+  <ul>
+    {todos.map((todo, index) => (
+      <Todo key={index} {...todo} onClick={() => onTodoClick(index)} />
+    ))}
+  </ul>
+  <span>Click on name to make it complete</span>
+  </div>
+);
+
+TodoList.propTypes = {
+  todos: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      completed: PropTypes.bool.isRequired,
+      text: PropTypes.string.isRequired
+    }).isRequired
+  ).isRequired,
+  onTodoClick: PropTypes.func.isRequired
+};
+
+export default TodoList
